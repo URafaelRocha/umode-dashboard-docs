e4f2ae596f75628559d2b49c9dab6bd7c3f5f9e7
Rafael Rocha <rafael.rocha@umode.com.br>
2024-11-06 10:43:59 -0300

    feat: og docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import Head from '@docusaurus/Head';
+
 export default function Home() {
   const history = useHistory();
 
@@ -8,5 +10,18 @@ export default function Home() {
     history.push('/docs');
   }, [history]);
 
-  return null;
+  return (
+    <Head>
+      <meta property="og:image" content="image.png" />
+      <script type="application/ld+json">
+        {JSON.stringify({
+          '@context': 'https://umode-dashboard-docs.vercel.app/docs',
+          '@type': 'uMode Dashboard Docs',
+          url: 'https://umode-dashboard-docs.vercel.app/',
+          name: 'uMode Dashboard Docs',
+          description: 'uMode Dashboard Docs',
+        })}
+      </script>
+    </Head>
+  );
 }
